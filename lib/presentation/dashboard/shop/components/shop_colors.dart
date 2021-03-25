86bc798307f71395867c7d2dc8e122aaac41adb6
Michael Olusegun <mikkyboy2005@gmail.com>
2021-03-25 16:52:36 +0100

    ShopPage added

---
+++ lib/presentation/dashboard/shop/components/shop_colors.dart
@@ -0,0 +1,7 @@
+import 'package:flutter/material.dart';
+
+class ShopColors {
+  static const orange = Color(0xFFf78909);
+  static const green = Color(0xFF04ab4c);
+  static const whiteSecond = Color(0xFFf8f8ff);
+}
